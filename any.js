b16464951b9e51a5684459b101b488bc254063c0
DevaJayantha <devajayantha888@gmail.com>
2022-02-22 22:12:52 +0800

    delete file js

--- any.js
+++
@@ -1,8 +0,0 @@
-/**
- * any type is same with variable not use type, ist mean any value can be assign.
- *
- * @see https://www.typescripttutorial.net/typescript-tutorial/typescript-any-type/
- */
-var result;
-result = 10.123;
-console.log(result.toFixed());
